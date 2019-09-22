457d1313ec00bf73f07c2d3c377a0006123f4f67
Esther <esther191299@gmail.com>
2019-09-22 21:22:24 +0200

    Begin_Code_Stub

---
+++ src/readingdatafromfiles/ReadingDataFromFiles.java
@@ -0,0 +1,19 @@
+package readingdatafromfiles; 
+ 
+import java.io.*; 
+ 
+public class ReadingDataFromFiles { 
+    public static void main(String[] args) throws IOException {
+    	
+        File outFile = new File("OutputFile.txt"); 
+        File inFile = new File("InputFile.txt"); 
+         
+        FileWriter out = new FileWriter(outFile); 
+        FileReader in = new FileReader(inFile); 
+ 
+        //Code Here... 
+         
+        out.close(); 
+        in.close(); 
+    } 
+} 
